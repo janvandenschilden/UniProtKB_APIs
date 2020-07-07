bc3527a8c5ed3257c9f89e157717e49796233db3
Jan Van den Schilden <jan.vandenschilden@gmail.com>
2020-07-07 10:33:28 +0200

    Finished code until UniRef50 member retrieval.

--- CompareProteinsEvolutionary.py
+++ CompareProteinsEvolutionary.py
@@ -12,10 +12,17 @@ class CompareProteinsEvolutionary:
             UniProtKB_idA,
             UniProtKB_idB,
             ):
+        # UniProtKb IDs
         self.idA = self._retrieve_id(UniProtKB_idA)
         self.idB = self._retrieve_id(UniProtKB_idB)
+        # UniRef50 IDs
         self.UniRef50_idA = self._retrieve_UniRef50_id(self.idA)
         self.UniRef50_idB = self._retrieve_UniRef50_id(self.idB)
+        # UniRef50 members and protein sequences
+        self.UniRef50_membersA_fasta = \
+            self._retrieve_UniRef50_members_fasta(self.UniRef50_idA)
+        self.UniRef50_membersB_fasta = \
+            self._retrieve_UniRef50_members_fasta(self.UniRef50_idB)
         
     def _retrieve_id(
             self,
@@ -72,6 +79,75 @@ class CompareProteinsEvolutionary:
                 Format="list",
                 ).strip()
         return UniRef50_id
+    
+    def _parse_fasta(
+            self,
+            fasta_string,
+            ):
+        """ Parse fasta string into dictionary.
+        
+        This function parses the fasta file (in string format) into a 
+        dictionary of protein sequences.  
+        
+        Parameters
+        ----------
+        fasta_string : str
+            A fasta file in string format.  
+        
+        Returns
+        -------
+        fasta_dictionary : dict
+            The fasta file in dictionary format.  
+                key : ID
+                value : protein sequence
+        """
+        fasta_dictionary = dict()
+        fasta_lines = (line for line in fasta_string.split("\n"))
+        for line in fasta_lines:
+            if line.startswith(">"):
+                Id = line.replace(">","")\
+                    .split("|")[1]
+                fasta_dictionary[Id] = ""
+            else:
+                fasta_dictionary[Id] += line.strip()
+        return fasta_dictionary
+    
+    def _retrieve_UniRef50_members_fasta(
+            self,
+            UniRef50_id,
+            ):
+        """Retrieve fasta file of UniRef50 members.
+        
+        All members of an UniRef50 cluster have a sequence identity of 
+        50% or more.  This means they are homologues to each other.  
+        The UniProtKB mapping API is used to retrieve those members in 
+        fasta format.  The resulting string is subsequently parsed to a 
+        dictionary.
+        
+        Parameters
+        ----------
+        UniRef50_id : str
+            The UniRef50 identifier.
+        
+        Returns
+        -------
+        UniRef50_members_fasta : dict
+            Returns a dictionary of the members and their protein 
+            sequence.  
+                key : UniProtKB ID 
+                value : protein sequence
+        """
+        # Retrieve fasta string with UniProtKB mapping service
+        UniRef50_members_fasta_str = UniProtKB_Mapping(
+                UniRef50_id,
+                From="NF50",
+                To="ACC",
+                Format="fasta",
+                )
+        # Parse string into dictionary of protein sequences
+        UniRef50_members_fasta = \
+            self._parse_fasta(UniRef50_members_fasta_str)
+        return UniRef50_members_fasta
         
     
     
@@ -85,5 +161,9 @@ print(
 print(
       C.UniRef50_idA,
       C.UniRef50_idB,
+      )
+print(
+      C.UniRef50_membersA_fasta,
+      C.UniRef50_membersB_fasta,
       )
       
